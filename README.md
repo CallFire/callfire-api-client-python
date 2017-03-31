83563d7dbe571891a262852c7013ddf4680f49fc
vladimir-mhl <vladimir.mhl@gmail.com>
2017-03-31 22:25:35 +0300

    minor fix

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 
 # CallFire API v2 Python client
 
-[![Build Status](https://travis-ci.org/CallFire/callfire-api-client-python.png?branch=master)](https://travis-ci.org/CallFire/callfire-api-client-python)
-[![Requires.io](https://requires.io/github/CallFire/callfire-api-client-python/requirements.svg?branch=master)](https://requires.io/github/CallFire/callfire-api-client-python/requirements?branch=master)
-
 See setup instructions at **[CallFire Developers Site](https://developers.callfire.com/callfire-api-client-python.html)**
--- callfire/client.py
+++ callfire/client.py
@@ -7,15 +7,19 @@ log = logging.getLogger(__name__)
 
 
 class CallfireClient:
-    def __init__(self, login, password):
+    def __init__(self, login, password, config=None):
+        self.config = {
+            'validate_responses': False
+        }
+        self.config.update({} if config is None else config)
+        print(self.config)
+        log.info('CallfireClient.config {0}', self.config)
         self.http_client = RequestsClient()
         self.http_client.set_basic_auth('www.callfire.com', login, password)
         self.swagger_client = SwaggerClient.from_url(
             spec_url=self.swagger_url(),
             http_client=self.http_client,
-            config={
-                'validate_responses': False
-            })
+            config=self.config)
 
     def __getattr__(self, item):
         return getattr(self.swagger_client, item)
